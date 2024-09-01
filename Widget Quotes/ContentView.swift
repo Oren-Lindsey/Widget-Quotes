f3befb4b818c67db5e0f87c0cf10f8f21d1fab76
Oren Lindsey <oren@lindsey.studio>
2024-09-01 13:25:19 -0400

    Initial Commit

---
+++ Widget Quotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Widget Quotes
+//
+//  Created by Oren Lindsey on 9/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Widget Quotes/Widget_QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Widget_QuotesApp.swift
+//  Widget Quotes
+//
+//  Created by Oren Lindsey on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Widget_QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
