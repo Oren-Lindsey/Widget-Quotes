9749125a4ea64beba2fbf9d5fad7924b4c98236a
Oren Lindsey <oren@lindsey.studio>
2025-02-26 14:22:32 -0500

    Added font size, font weight, title, and preview to the editor

---
+++ Widget Quotes/WidgetDetail.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetDetail.swift
+//  Widget Quotes
+//
+//  Created by Oren Lindsey on 2/24/25.
+//
+
+import SwiftUI
+
+struct WidgetDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetDetail()
+}
